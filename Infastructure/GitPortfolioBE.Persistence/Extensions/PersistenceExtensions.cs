2893b87c3e26c20c4777a52532f63f4053675c71
onurabdulaji <onurabdulaji@gmail.com>
2025-03-16 22:41:05 +0100

    Migration And Persistence Extension and Connection String

---
+++ Infastructure/GitPortfolioBE.Persistence/Extensions/PersistenceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using GitPortfolioBE.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GitPortfolioBE.Persistence.Extensions;
+
+public static class PersistenceExtensions
+{
+    public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+}
